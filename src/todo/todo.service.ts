e7a99477176f0e8fb6c1a7b71ae1bafb12633e5f
coderegtech <reygundanag55@gmail.com>
2023-05-19 18:37:43 +0800

    Fixed todo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -11,11 +11,7 @@ export class TodoService {
   async create(createTodoInput: CreateTodoInput) {
     try {
 
-<<<<<<< HEAD
       return await this.prisma.todo.create({
-=======
-       return await this.prisma.todo.create({
->>>>>>> fb9cba0524fd21d71c796f66ad9bd7ad25bd2657
         data: {
           todoText: createTodoInput.todoText,
           completed: createTodoInput.completed
